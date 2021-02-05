3d78048218c52a7a7f1748e0c427b5761cf72cc4
tsokac2 <tsokac2@gmail.com>
2021-02-05 05:59:31 +0000

    add

--- run.py
+++ run.py
@@ -1,5 +1,19 @@
 import os
+from flask import Flask
+from flask_pymongo import PyMongo
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 from newirishlife import app
+from newirishlife import routes
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
